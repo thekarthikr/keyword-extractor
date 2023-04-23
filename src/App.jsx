93cfa7776a7977c17ee48b43f4696e9fed7ebdef
Karthik <karthikwebdev@outlook.com>
2023-04-23 17:44:19 +0530

    add error  message

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Container, Box } from "@chakra-ui/react";
+import { Container, Box, useToast } from "@chakra-ui/react";
 import Header from "./components/Header";
 import TextInput from "./components/TextInput";
 import KeywordsModal from "./components/KeywordsModal";
@@ -8,6 +8,8 @@ const App = () => {
   const [keywords, setKeywords] = useState("");
   const [isOpen, setIsOpen] = useState(false);
   const [loading, setLoading] = useState(false);
+
+  const toast = useToast();
 
   const extractKeywords = async (text) => {
     setLoading(true);
@@ -24,24 +26,34 @@ const App = () => {
       "${text}","top_n":20,"ngram_range":[1,2],"diversify":false,"diversity":0}`,
     };
 
-    const res = await fetch(
-      "https://keyword-extractor.p.rapidapi.com/api/keyword",
-      options
-    );
+    try {
+      const res = await fetch(
+        "https://keyword-extractor.p.rapidapi.com/api/keyword",
+        options
+      );
+      const { result } = await res.json();
 
-    const { result } = await res.json();
+      let words = Object.keys(result);
 
-    let words = Object.keys(result);
-
-    let capitalizedWords = words.map(function (word) {
-      return word.replace(/\b\w/g, function (firstLetter) {
-        return firstLetter.toUpperCase();
+      let capitalizedWords = words.map(function (word) {
+        return word.replace(/\b\w/g, function (firstLetter) {
+          return firstLetter.toUpperCase();
+        });
       });
-    });
-    const data = capitalizedWords.join(", ");
+      const data = capitalizedWords.join(", ");
 
-    setKeywords(data);
-    setLoading(false);
+      setKeywords(data);
+      setLoading(false);
+    } catch (error) {
+      toast({
+        title: "Something went wrong",
+        description: "Please try again later",
+        status: "error",
+        duration: 4000,
+        isClosable: true,
+      });
+      closeModal();
+    }
   };
 
   const closeModal = () => {
