e7372050fb0caf5912b09eef554e937ba02c08ef
Karthik <karthikwebdev@outlook.com>
2023-04-23 14:32:02 +0530

    Add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import { Box, Text } from "@chakra-ui/react";
+
+const Footer = () => {
+  return (
+    <Box marginTop={50}>
+      <Text>Powered by Open AI</Text>
+    </Box>
+  );
+};
+
+export default Footer;
