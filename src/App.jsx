31c2cd0f094246f99f37ae8cace74a5337d51539
Karthik <karthikwebdev@outlook.com>
2023-04-23 14:34:57 +0530

    import all the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import { Container, Box } from "@chakra-ui/react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import TextInput from "./components/TextInput";
 
 const App = () => {
+  const extractKeywords = (text) => {};
+
   return (
-    <>
-      <h1> Keyword Extractor</h1>
-    </>
+    <Box background={"gray.200"} height='100vh' paddingTop={"5rem"}>
+      <Container maxW={"3xl"} centerContent>
+        <Header />
+        <TextInput extractKeywords={extractKeywords} />
+        <Footer />
+      </Container>
+    </Box>
   );
 };
 
